2c85016c4775cf1618229c3d96769cf85c73dc23
Miro Laukka <60649670+mirolaukka@users.noreply.github.com>
2020-07-21 10:28:39 +0300

    :white_check_mark: Removed a commented line

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ let port = process.env.PORT;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
-//app.use(express.static(path.join(__dirname, "public")));
 
 
 /*
